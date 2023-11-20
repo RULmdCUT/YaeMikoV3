8d52adb771f920d48e7f371415934e264be93dff
RULmdCUT <149047688+RULmdCUT@users.noreply.github.com>
2023-11-20 16:00:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -53,23 +53,23 @@ global.snh = 'https://nhentai.net/g/365296/'
 //mwehehe:v
 
 /*============== PAYMENT ==============*/
-global.pdana = '085731947500'
+global.pdana = '085707733624'
 global.povo = '-'
-global.pgopay = '085731947500'
-global.ppulsa = '085731947500'
-global.ppulsa2 = '085749543572'
+global.pgopay = '085235389428'
+global.ppulsa = '085235389428'
+global.ppulsa2 = '085235389428'
 global.psaweria = 'https://saweria.co.id/oktakc'
 global.fetch = (await import('node-fetch'))
 /*============== NOMOR ==============*/
-global.nomorbot = '6285749543572'
-global.nomorown = '6285731947500'
+global.nomorbot = '6285706375288'
+global.nomorown = '6285706375288'
 global.namebot = 'YaeMiko'
 global.nameown = 'Okta'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6285731947500', 'Okta🗿!', true]
+  ['6285706375288', 'Okta🗿!', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
